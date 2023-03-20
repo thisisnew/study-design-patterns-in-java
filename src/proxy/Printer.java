96f98218a7dc1da79e4abe9f04ff4a538971d012
thisisnew <thisisnew@naver.com>
2023-03-21 08:49:24 +0900

    Proxy Pattern

--- src/proxy/Printer.java
+++ src/proxy/Printer.java
@@ -1,18 +1,46 @@
 package proxy;
 
 public class Printer implements Printable {
+
+    private String name;
+
+    public Printer() {
+        heavyJob("Printer의 인스턴스를 생성 중");
+    }
+
+    public Printer(String name) {
+        this.name = name;
+        heavyJob("Printer의 인스턴스 (" + name + ")을 생성 중");
+    }
+
     @Override
     public String getPrinterName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setPrinterName(String name) {
-
+        this.name = name;
     }
 
     @Override
     public void print(String string) {
+        System.out.println("===" + name + "===");
+        System.out.println(string);
+    }
 
+    private void heavyJob(String msg) {
+        System.out.print(msg);
+        for (int i = 0; i < 5; i++) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+
+            System.out.print(".");
+        }
+        System.out.println("완료.");
     }
 }
