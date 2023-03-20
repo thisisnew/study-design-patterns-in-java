9b4330084f309e127dc163d3841e0589f8b610b7
thisisnew <thisisnew@naver.com>
2023-03-20 23:32:55 +0900

    Proxy Pattern

---
+++ src/proxy/Printable.java
@@ -0,0 +1,10 @@
+package proxy;
+
+public interface Printable {
+    String getPrinterName();
+
+    void setPrinterName(String name);
+
+    void print(String string);
+}
+
---
+++ src/proxy/Printer.java
@@ -0,0 +1,18 @@
+package proxy;
+
+public class Printer implements Printable {
+    @Override
+    public String getPrinterName() {
+        return null;
+    }
+
+    @Override
+    public void setPrinterName(String name) {
+
+    }
+
+    @Override
+    public void print(String string) {
+
+    }
+}
